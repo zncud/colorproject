2791240910c692c731ca0a826af5c13b16f93cc3
koko <kokomeron1028@gmail.com>
2021-03-06 23:25:57 +0900

    fixed conflict

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    before_save {email.downcase!}
+    validates :name, presence: true, uniqueness: true, length: {maximum: 50}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum: 255},
+                        format: {with: VALID_EMAIL_REGEX}
+    has_secure_password
+    validates :password, presence: true , length:{minimum: 6}
+end
