9ae6c719eece99f1ac06c4b5c8162f8894e27020
koko <kokomeron1028@gmail.com>
2021-03-06 23:15:41 +0900

    Make a basic User model

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
   end
 
   test "should get new" do
-    get static_pages_new_url
+    get users_new_path
     assert_response :success
   end
 
